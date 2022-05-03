233f11511c61ab7baadb0d3b38ad30a6c706a875
arlw87 <arlw87@googlemail.com>
2022-05-03 14:04:55 +0100

    Initialise Spring MVC project

---
+++ src/test/java/com/code/arlw87/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.arlw87.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
